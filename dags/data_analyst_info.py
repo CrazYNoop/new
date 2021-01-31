fec316684996eb9e2f2180dc868988495d027909
Mikhail <sletovmisha@gmail.com>
2021-01-31 04:10:12 -0800

    Changed dag file

--- dags/data_analyst_info.py
+++ dags/data_analyst_info.py
@@ -12,39 +12,37 @@ args = {
     'depends_on_past': False,
 }
 
-def download_data_from_hh():
+def data_analyst_info():
     vacancies = []
     vacancy_query = "data analyst"
     for page_number in range(50):
         response = requests.get("https://api.hh.ru/vacancies",
-                       params = {
-                           "text": vacancy_query,
-                           "page": page_number
-})
-    response_json=response.json()
-    vacancies+=response_json['items']
+                           params = {
+                               "text": vacancy_query,
+                               "page": page_number
+    })
+        response_json=response.json()
+        vacancies+=response_json['items']
 
     vacancies_dataset = {
-    "vacancy_name": [],
-    "city": []
-}
+        "vacancy_name": [],
+        'employer':[],
+        "city": []
+    }
 
     for vacancy in vacancies:
         vacancies_dataset["vacancy_name"].append(vacancy["name"])
+        vacancies_dataset["employer"].append(vacancy["employer"]['name'])
         vacancies_dataset["city"].append(vacancy["area"]["name"])
     vacancies_dataset = pd.DataFrame(vacancies_dataset)
-    df=vacancies_dataset['city'].value_counts()    
-    df.to_csv(os.path.join(os.path.expanduser('~'), 'data_analyst.csv'))
+    df=vacancies_dataset['city'].value_counts()
+    df.to_csv('data_analyst_city_count.csv')
+    vacancies_dataset.to_csv('data_analyst.csv')
+    print("Files saved Successfully")
 
-with DAG(dag_id='data_analyst_info', default_args=args, schedule_interval=@daily) as dag:
-    take_data_hh = PythonOperator(
-        task_id='download_data_from_hh',
-        python_callable=download_data_from_hh,
+with DAG(dag_id='data_analyst_info', default_args=args, schedule_interval=None) as dag:
+    data_analyst_info = PythonOperator(
+        task_id='data_from_hh_api',
+        python_callable=data_analyst_info,
         dag=dag
     )
-    say_hello = PythonOperator(
-        task_id='say_hello',
-        python_callable="print('Hello airflow')",
-        dag=dag
-    )
-    take_data_hh >> say_hello
